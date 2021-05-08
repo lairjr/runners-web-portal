302e57209b0c6f0957f857ba008ba37584829e34
Lair Júnior <lairjr@outlook.com>
2021-05-08 17:16:45 -0400

    Add list route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import List from './routes/List';
 
 function App() {
   return (
     <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <List />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
